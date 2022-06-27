13fb971f34566bfc1541178261e213034977112a
Salt Dev <saltdev@dev138.local>
2022-06-27 22:34:00 +0200

    learning react

---
+++ react-webapp1/src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doc appt',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Vet appt',
+        day: 'Feb 10th at 6:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Hair appt',
+        day: 'March 15th at 12:30pm',
+        reminder: false,
+    }
+])
+
+// delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter(task => task.id !== id))
+}
+
+  return (
+    <div className="container">
+      <Header title='Tasks'/>
+      {tasks.length > 0 ? 
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
+        : ("Create a new task")
+      }
+    </div>
+    
+  );
+}
+
+export default App;
